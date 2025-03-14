e3ae16398560613668945f37b587d4b9c8b2d2f7
JasN07 <jasalynnmonique07@gmail.com>
2025-03-13 22:33:57 -0700

    yayy

--- js/sound.js
+++ js/sound.js
@@ -3,19 +3,19 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
-    btn.innerHTML = sound
+    btn.innerText=sound
 btn.addEventListener('click', ()=> {
     stopSongs()
 document.getElementById(sound).play()
 })
-
     document.getElementById('buttons').appendChild(btn)
 })
 
 
 function stopSongs(){
 sounds.forEach(sound => {
-    document.getElementById(sound).pause()
-    document.getElementById(sound)currentTime = 0;
+    song = document.getElementById(sound)
+    song.pause()
+    song.currentTime = 0;
 })
 }
