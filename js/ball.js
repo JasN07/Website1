3f30c526f9b7e547ec556ec0b6ee20c23b638f3a
JasN07 <jasalynnmonique07@gmail.com>
2025-04-08 16:35:52 +0000

    Tue, Apr 8, 2025, 9:35 AM -07:00

---
+++ js/ball.js
@@ -0,0 +1,67 @@
+const ball = document.createElement('div')
+const ballRadius = 30
+const windowHeight = window.innerHeight
+const windowWidth = window.innerWidth
+let ballYPosition = windowWidth/2 - ballRadius
+let ballXPosition = windowWidth/2 - ballRadius
+document.body.appendChild(ball)
+const Lpadel = document.
+let ballSpeed = 5
+let ballxDirection =
+const LPadel = document.createElement('div')
+document.body.appendChild(LPadel)
+let LPadelWidth = 20
+let LPadelHeight = 100
+let LPadelSpeed = 5
+let
+
+setInterval(moveBall, 10)
+
+function moveBall(){
+    ballXPosition = ballXPosition + ballSpeed * ballXDirection
+    ballYPosition = ballYPosition + ballSpeed * ballYDirection
+    ball.style.left = `${ballXPosition}px'
+    ball.style.top = `${ballYPosition}px`
+    if (ballYPosition < 0 || ballYPosition > windowWidth - 2 * ballRadius){
+    ballYDirection = ballYDirection * -1
+    }
+    if (ballXPosition < 0 || ballXPosition > windowWidth - 2 * ballRadius){
+    ballXDirection = ballXDirection * -1
+    }
+}
+
+
+
+createBall()
+
+function createBall(){
+    ball.style.height = `${2 * ballRadius}`
+    ball.style.width = `${2 * ballRadius}`
+    ball.style.borderRadius = "50%"
+    ball.style.backgroundColor = "Lavender"
+    ball.style.position = "absolute"
+    ball.style.top = `${windowHeight/2 - ballRadius}px`
+    ball.style.left = `${windowWidth/2 - ballRadius}px`
+}
+
+create LPadel() {
+LPadel.style.height = `${LPadelHeight}px`
+LPadel.style.width = `${LPadelWidth}px`
+LPadel.style.backgroundColor = 'pink'
+LPadel.style.position = 'absolute'
+LPadel.style.left = "50px"
+LPadel.style.top = `${windowHeight / 2 - LPadelHeight / 2}px`
+}
+
+LPadel.addEventListener('keyup', (event) => {
+    if(event.key == 'w') {
+    if(LPadel)
+    LPadelYPosition = LPadelYPosition - LPadelSpeed
+    }
+    if(event.key == 's') {
+        if(LPadelYPosition >= windowHeight - LPadelHeight) {
+            LPadelYPosition = LPadelYPosition - LPadelHeight
+    }
+    }
+    LPadel.style.top = `${LPadelYPosition}px`
+    })
