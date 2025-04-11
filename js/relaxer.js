f53e476e3b10db57ec945cc1aba58e2e959a9165
JasN07 <jasalynnmonique07@gmail.com>
2025-04-10 23:07:56 -0700

    relaxer

--- js/relaxer.js
+++ js/relaxer.js
@@ -1,1 +1,20 @@
-const container = document.getElement
+const container = document.getElementById('container')
+const text = document.getElementById('text')
+
+setInterval(breatheAnimation, 7500)
+
+function breatheAnimation() {
+    text.innerHTML = 'Breathe In'
+    container.classList.remove('shrink')
+    container.classList.add('grow')
+
+    setTimeout( () => {
+        text.innerHTML = 'Hold'
+    }, 3000)
+
+    setTimeout( () => {
+        text.innerHTML = 'Breathe Out'
+        container.classList.remove('grow')
+        container.classList.add('shrink')
+    }, 4500)
+}
